181edd3b43016b4b5961719b1af8da81f3b4a397
Ema Forlin <emaa.forlin@gmail.com>
2024-11-27 09:52:07 -0300

    navbar component span the full width if the screen

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,7 +3,7 @@ export function NavBar() {
   return (
     <>
     <div className="bg-green-200 text-gray-800 sticky top-0 z-10">
-      <div className="container mx-auto  px-5 py-4">
+      <div className="px-5 py-4">
         <nav className="flex justify-between items-center">
           <h2 className="flex justify-center font-bold text-2xl text-slate-800">Coupons App</h2>
           <ul className="flex gap-6 justify-center font-bold text-xl text-green-700">
